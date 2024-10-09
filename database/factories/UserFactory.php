c2fd40ae52ac623562327c70a36eb46e57a1b96c
thiago-NSampaio <110494178+thiago-NSampaio@users.noreply.github.com>
2024-10-09 15:12:23 -0400

    create models user and project

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class UserFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'rating' => fake()->randomElement([1,2,3,4,5]),
+            'avatar' => 'https://avatar.iran.liara.run/public'
+        ];
+    }
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Models\Project;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::factory()->count(100)->create();
+        User::query()->inRandomOrder()->limit(10)->get()
+            ->each(fn(User $u) => Project::factory()->create(['created_by' => $u->id]));
+    }
+}
